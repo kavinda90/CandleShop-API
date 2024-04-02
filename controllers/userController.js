d9b0c2d1017ac07fb9462c5160ee7e3629e6d542
chanaka <chanakadrathnayaka@gmail.com>
2024-04-01 22:44:30 -0700

    User profile related APIs are updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,8 +7,7 @@ const getValidUserByEmail = async (req, res) => {
     if (!user) {
       res.status(401).json({message: 'Your email is wrong'});
     } else if (user.password === encryptPassword(req.body.password)) {
-      const sanitizedUser = await getSanitizedUser(user._id);
-      res.status(200).json(sanitizedUser);
+      res.status(200).json(await getSanitizedUser(user._id));
     } else {
       res.status(401).json({message: 'Your password doesn\'t match'});
     }
@@ -23,7 +22,7 @@ const createUser = async (req, res) => {
   });
   try {
     const newUser = await user.save();
-    res.status(201).json(getSanitizedUser(newUser._id));
+    res.status(201).json(await getSanitizedUser(newUser._id));
   } catch (error) {
     res.status(400).json({message: error.message});
   }
@@ -31,13 +30,35 @@ const createUser = async (req, res) => {
 
 const updateUserByEmail = async (req, res) => {
   try {
-    const updatedUser = await User
-    .findOneAndUpdate({email: req.body.email}, req.body);
-    if (updatedUser) {
-      res.status(200).json(getSanitizedUser(updatedUser._id));
-    } else {
+    const existingUser = await User.findOne({email: req.body.email});
+
+    if (!existingUser) {
       res.status(404).json({message: 'User not found'});
+      return;
     }
+    const {firstName, lastName, email, phone, address, password} = req.body;
+
+    if (firstName && existingUser.firstName !== firstName) {
+      existingUser.firstName = firstName;
+    }
+    if (lastName && existingUser.lastName !== firstName) {
+      existingUser.lastName = lastName;
+    }
+    if (email && existingUser.email !== email) {
+      existingUser.email = email;
+    }
+    if (phone && existingUser.phone !== phone) {
+      existingUser.phone = phone;
+    }
+    if (address && existingUser.address !== address) {
+      existingUser.address = address;
+    }
+    if (password && existingUser.password !== password) {
+      existingUser.password = password;
+    }
+
+    const updatedUser = await existingUser.save();
+    res.status(200).json(await getSanitizedUser(updatedUser._id));
   } catch (error) {
     res.status(500).json({message: error.message});
   }
@@ -45,7 +66,7 @@ const updateUserByEmail = async (req, res) => {
 
 const getSanitizedUser = async (id) => {
   return User
-  .findById(id).select('-_id -__v -password');
+  .findById(id).select('-_id -__v -password').lean();
 }
 
 const encryptPassword = (password) => {
